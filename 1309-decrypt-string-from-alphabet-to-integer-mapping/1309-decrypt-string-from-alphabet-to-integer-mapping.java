ca37a0fc972f37660a685504000fc552a1a4fdf6
Shubham Sigdar <107163260+shubhamsigdar1@users.noreply.github.com>
2022-12-05 20:12:22 +0530

    Time: 1 ms (96.88%), Space: 42.2 MB (61.81%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.java
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.java
@@ -1,20 +1,16 @@
 class Solution {
     public String freqAlphabets(String s) {
-        // StringBuilder s1 = new StringBuilder();
-        StringBuilder output = new StringBuilder();
+        StringBuilder s1 = new StringBuilder();
         for(int i=s.length()-1;i>=0;i--){
-            // if(s.charAt(i)=='#'){
-            //     s1.append((char)('a'+(s.charAt(i-1)-'0') + 10*(s.charAt(i-2)-'0')-1));
-            if(s.charAt(i) == '#'){
-                output.append((char)('a' + (s.charAt(i-1) -'0' ) + 10*(s.charAt(i-2)-'0') -1) );
+            if(s.charAt(i)=='#'){
+                s1.append((char)('a'+(s.charAt(i-1)-'0') + 10*(s.charAt(i-2)-'0')-1));
                 i-=2;
             }else{
-                // s1.append((char)('a' + (s.charAt(i)-'0')-1));
-                 output.append((char)('a' + (s.charAt(i) - '0') -1));
+                s1.append((char)('a' + (s.charAt(i)-'0')-1));
+                 // output.append((char)('a' + (s.charAt(i) - '0') -1));
             }
         }
-        output.reverse(); //very helpful method, reverses the whole string
-        
-        return output.toString(); //convert StringBuilder obj to string
+        s1.reverse();
+        return s1.toString();
     }
 }
