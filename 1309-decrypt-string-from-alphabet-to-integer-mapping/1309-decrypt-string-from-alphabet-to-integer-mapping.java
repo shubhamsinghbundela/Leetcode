f428b2a80aa753bc48c424325b013e59c84c5141
Shubham Sigdar <107163260+shubhamsigdar1@users.noreply.github.com>
2022-12-05 20:11:50 +0530

    Time: 2 ms (82.81%), Space: 41.8 MB (80.47%) - LeetHub

---
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String freqAlphabets(String s) {
+        // StringBuilder s1 = new StringBuilder();
+        StringBuilder output = new StringBuilder();
+        for(int i=s.length()-1;i>=0;i--){
+            // if(s.charAt(i)=='#'){
+            //     s1.append((char)('a'+(s.charAt(i-1)-'0') + 10*(s.charAt(i-2)-'0')-1));
+            if(s.charAt(i) == '#'){
+                output.append((char)('a' + (s.charAt(i-1) -'0' ) + 10*(s.charAt(i-2)-'0') -1) );
+                i-=2;
+            }else{
+                // s1.append((char)('a' + (s.charAt(i)-'0')-1));
+                 output.append((char)('a' + (s.charAt(i) - '0') -1));
+            }
+        }
+        output.reverse(); //very helpful method, reverses the whole string
+        
+        return output.toString(); //convert StringBuilder obj to string
+    }
+}
