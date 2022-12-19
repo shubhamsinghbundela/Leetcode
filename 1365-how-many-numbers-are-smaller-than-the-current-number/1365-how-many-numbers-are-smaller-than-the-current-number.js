5a9d6e79348c51f912fb26841ae0533d0a655cb1
Shubham Sigdar <107163260+shubhamsigdar1@users.noreply.github.com>
2022-12-19 09:40:50 +0530

    Time: 112 ms (69.52%), Space: 43.9 MB (81.00%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -3,15 +3,8 @@
  * @return {number[]}
  */
 var smallerNumbersThanCurrent = function(nums) {
-  let nums1=[];
-    for(let i=0;i<nums.length;i++){
-      let count=0;
-      for(let j=0;j<nums.length;j++){
-        if(nums[i]>nums[j]){
-          count++;
-        }
-      }
-      nums1.push(count);
-    }
-  return nums1;
+  let sorted=[...nums].sort((a,b)=>a-b);
+  // console.log(sorted)
+  let nums1=nums.map(num=>sorted.indexOf(num))
+  return nums1
 };
