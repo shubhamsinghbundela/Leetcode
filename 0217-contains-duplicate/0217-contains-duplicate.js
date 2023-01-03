1cd07d10b7f5588fc8498df534a19b5acee73043
Shubham Sigdar <107163260+shubhamsigdar1@users.noreply.github.com>
2023-01-03 10:13:18 +0530

    Time: 1489 ms (12.99%), Space: 48.5 MB (89.89%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
+            if(nums[i]==nums[j]){
+                return true;
+                break;
+            }
+        }
+    }
+    return false;
+};
