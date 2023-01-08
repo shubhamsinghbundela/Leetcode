924765710460b93a577ed897b8beffcba53d6237
Shubham Sigdar <107163260+shubhamsigdar1@users.noreply.github.com>
2023-01-08 13:21:13 +0530

    Time: 63 ms (97.68%), Space: 43.1 MB (30.94%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,11 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for(let i=0;i<nums.length;i++){
-      for(j=i+1;j<nums.length;j++){
-        if(nums[i]+nums[j]===target){
-          return [i,j];
-        }
-      }
+  let map={};
+  for(let i=0;i<nums.length;i++){
+    let another=target-nums[i];
+    if(another in map){
+      return [map[another],i]
+    }else{
+      map[nums[i]]=i;
     }
+  }
+  
 };
