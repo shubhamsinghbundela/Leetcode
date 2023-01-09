57271687183ccae89b564442a805a284f1e5bda9
Shubham Sigdar <107163260+shubhamsigdar1@users.noreply.github.com>
2023-01-09 09:56:40 +0530

    Time: 69 ms (84.67%), Space: 42.3 MB (76.73%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -3,20 +3,57 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    if(strs==='undefined' && strs.length===0){
-      return ''
+    
+    let pre = strs[0];
+    
+    for(let word of strs) {
+                
+        for(let i = pre.length - 1; i >= 0; i--) {
+                             
+            if(pre[i] !== word[i]) {
+                pre = pre.slice(0, i);
+            }
+        }
     }
-    let str1=strs.map(e=>e.length)
-    let smallLetterString=strs[str1.indexOf(Math.max(...str1))]
-    let prefix='';
-    // return smallLetterString
-    for(let i=0;i<smallLetterString.length;i++){
-      let char=strs[0][i];
-      if(strs.every(e=>e[i]===char)){
-         prefix+=char
-       }else{
-         break;
-       }
-    }
-  return prefix
+    
+    return pre;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
