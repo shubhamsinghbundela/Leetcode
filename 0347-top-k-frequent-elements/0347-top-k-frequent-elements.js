d5f339a1e96f88519acf904d4a16601e136a10ef
Shubham Sigdar <107163260+shubhamsigdar1@users.noreply.github.com>
2023-01-30 10:23:49 +0530

    Time: 100 ms (43.93%), Space: 46.1 MB (38.84%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    const freqMap = new Map();
+    const bucket = [];
+    const result = [];
+    
+    for(let num of nums) {
+        freqMap.set(num, (freqMap.get(num) || 0) + 1);
+    }
+    
+    for(let [num, freq] of freqMap) {
+        bucket[freq] = (bucket[freq] || new Set()).add(num);
+    }
+  // console.log(bucket)
+    
+    for(let i = bucket.length-1; i >= 0; i--) {
+        if(bucket[i]) result.push(...bucket[i]);
+        if(result.length === k) break;
+    }
+    return result;
+};
