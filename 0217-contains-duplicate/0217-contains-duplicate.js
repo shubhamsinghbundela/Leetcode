e876a121e929e01dfc38f9b9f422b88da10ab4a8
Shubham Sigdar <107163260+shubhamsigdar1@users.noreply.github.com>
2023-02-05 19:22:53 +0530

    Time: 181 ms (26.51%), Space: 52.2 MB (31.72%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,23 +3,12 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-  let obj={};
-  // let count=0;
-  nums.forEach(e=>{
-    if(e in obj){
-      obj[e]+=1;
-    }else{
-      obj[e]=1;
-    }
-  })
-  
-  for(let key in obj){
-    if(obj[key]>1){
+ nums.sort((a, b) => a - b); // this sort manipulates the original array.
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
       return true;
-      break;
     }
   }
-  
   return false;
   
 };
