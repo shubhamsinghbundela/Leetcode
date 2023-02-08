59bcef6436c0ef631d0d5df724d64d04333f3dd6
Shubham Sigdar <107163260+shubhamsigdar1@users.noreply.github.com>
2023-02-08 09:37:24 +0530

    Time: 67 ms (89.92%), Space: 45.7 MB (5.17%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,15 +3,29 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-  let map={};
-  for(let i=0;i<nums.length;i++){
-    let another=target-nums[i];
-    if(another in map){
-      return [map[another],i]
-    }else{
-      map[nums[i]]=i;
-    }
+var twoSum = (nums, target) => {
+  const map = getMap(nums);       
+  // console.log(getMap(nums))
+  return getSum(nums, target, map)
+}
+
+const getMap = (nums, map = new Map()) => {
+  for (let index = 0; index < nums.length; index++) {
+      map.set(nums[index], index);                        
   }
   
-};
+  return map
+   
+}
+
+const getSum = (nums, target, map) => {
+  for (let index = 0; index < nums.length; index++) {
+      const complement = target - nums[index];
+      const sumIndex = map.get(complement);
+
+      const isTarget = map.has(complement) && (map.get(complement) !== index)
+      if (isTarget) return [ index, sumIndex ]
+  }
+
+  return [ -1, -1 ];
+}
