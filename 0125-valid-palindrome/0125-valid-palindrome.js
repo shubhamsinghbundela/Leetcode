ba208dd0397197e9f64181b6e6765e8c40d823cd
Shubham Sigdar <107163260+shubhamsigdar1@users.noreply.github.com>
2023-02-09 08:53:21 +0530

    Time: 70 ms (89.86%), Space: 48.4 MB (24.36%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -4,22 +4,33 @@
  */
 //1. convert uppercase into lowercase(done)
 //2. removing all non-alphanumeric characters
+//1. convert uppercase into lowercase(done)
+//2. removing all non-alphanumeric characters
 var isPalindrome = function(s) {
-  let s1=s.toLowerCase()
-    // console.log(s1)
-  var regexPattern = /[^A-Za-z0-9]/g;
-  var ans = s1.replace(regexPattern, "");
-  console.log(ans)
-  let temp="";
-  for(let i=ans.length-1;i>=0;i--){
-    temp+=ans[i]
-  }
-  if(ans==temp){
+  if(s.length<1){
     return true;
-  }else{
-    return false;
   }
-    
+  let[left,right]=[0,s.length-1];
+  // console.log(left)
+  let leftChar,rightChar;
+  while(left<=right){
+     leftChar=s[left];
+     rightChar=s[right];
+    if(!/[A-Za-z0-9]/.test(leftChar)){
+      left++
+    }else if(!/[A-Za-z0-9]/.test(rightChar)){
+      right--
+    }else {
+            // compare letters
+            if (leftChar.toLowerCase() != rightChar.toLowerCase()) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+  }
+  return true;
     
 };
+// console.log(isPalindrome("A man, a plan, a canal: Panama"))
 
