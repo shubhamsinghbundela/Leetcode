8df645b24d7026814a3ba0f758c351743540b103
Shubham Sigdar <107163260+shubhamsigdar1@users.noreply.github.com>
2023-02-16 09:35:25 +0530

    Time: 72 ms (73.41%), Space: 42.7 MB (77.60%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.js
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -3,33 +3,28 @@
  * @return {number}
  */
 var trap = function(height) {
-  let leftWater=[];
-  let rightWater=[];
-  let leftRightWater=[];
-  let current=0
-  for(let i=0;i<height.length;i++){
-    leftWater.push(current)
-    current=Math.max(current,height[i])
-  }
-  console.log(leftWater)
-  current=0
-  for(let i=height.length-1;i>-1;i--){
-    rightWater.push(current)
-    current=Math.max(current,height[i])
-  }
-  // console.log(rightWater)
-  rightWater.reverse()
-  console.log(rightWater)
-  for(let i=0;i<height.length;i++){
-    leftRightWater[i]=Math.min(leftWater[i],rightWater[i])
-  }
-  console.log(leftRightWater)
-  let water=0
-  for(let i=0;i<height.length;i++){
-    if(leftRightWater[i]-height[i]>0){
-      water+=leftRightWater[i]-height[i]
+  let left=0;
+  let right=height.length-1;
+  let maxL=0
+  let maxR=0
+  let res=0
+  while(left<=right){
+    if(height[left]<=height[right]){
+      if(height[left]>=maxL){
+        maxL=height[left]
+      }else{
+        res+=maxL-height[left]
+      }
+      left++;
+    }
+    else{
+      if(height[right]>=maxR){
+        maxR=height[right]
+      }else{
+        res+=maxR-height[right]
+      }
+      right--
     }
   }
-  return water
-  
+  return res;
 };
