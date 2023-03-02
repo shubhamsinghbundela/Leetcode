ce9d566e9ad3db3fe3f47a8e995e58bcacc7d198
Shubham Sigdar <107163260+shubhamsigdar1@users.noreply.github.com>
2023-03-02 13:30:14 +0530

    Time: 127 ms (64.74%), Space: 50.7 MB (91.13%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -2,31 +2,26 @@
  * @param {number[]} nums
  * @return {number}
  */
-
 var longestConsecutive = function(nums) {
-  if(!nums.length){
-    return 0;
+  let maxScore=1;
+  let count=1;
+  if(nums.length==0){
+      return 0;
   }
   let nums1=nums.sort((a,b)=>a-b)
-  // console.log(nums1)
-  return search(nums1)
-}
-var search = function(nums1){
-  let [maxScore,score]=[1,1]
-  for(let i=0;i<nums1.length;i++){
-    let prev=nums1[i]===nums1[i-1]
-    if(prev){
+  console.log(nums1)
+  for(let i=1;i<nums1.length;i++){
+    if(nums[i]==nums[i-1]){
+      continue;
+    } 
+    if(nums[i]==nums[i-1]+1){
+      count+=1
       continue;
     }
-    let newScore=nums1[i]===nums1[i-1]+1
-    if(newScore){
-      score++;
-      continue;
-    }
-    maxScore=Math.max(maxScore,score);
-    score=1
+    maxScore=Math.max(maxScore,count);
+    count=1
+    
   }
-  return Math.max(maxScore, score);
-}
-
-
+  return Math.max(maxScore,count)
+  
+};
