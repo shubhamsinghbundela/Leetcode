f9b96307c18feb443658faa4373f527a9f7e242f
Shubham Sigdar <107163260+shubhamsigdar1@users.noreply.github.com>
2023-04-23 09:55:05 +0530

    Time: 64 ms (74.49%), Space: 43.7 MB (75.00%) - LeetHub

---
+++ 0832-flipping-an-image/0832-flipping-an-image.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} image
+ * @return {number[][]}
+ */
+var flipAndInvertImage = function(image) {
+
+    for(let i=0;i<image.length;i++){
+      image[i].reverse();
+      for(let j=0;j<image[i].length;j++){
+      image[i][j]=+!(image[i][j])
+      }
+    }
+
+    return image;
+};
