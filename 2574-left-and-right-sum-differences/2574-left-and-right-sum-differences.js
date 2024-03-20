05f2e4fa4abeb6dabb21c95c88313439fbf85ab6
Shubham Kumar Singh <107163260+shubhamsinghbundela@users.noreply.github.com>
2024-03-21 00:23:05 +0530

    Time: 58 ms (89.01%), Space: 52.3 MB (52.75%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.js
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.js
@@ -2,21 +2,22 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var leftRigthDifference = function(nums) {
-  let leftSum =[0];
-  for(let i=1;i<nums.length;i++){
-    leftSum[i]= leftSum[i-1] + nums[i-1];
-  }
-  // console.log(leftSum)
-  let rightSum =[];
+var leftRightDifference = function(nums) {
+  let leftSum=[];
+  leftSum[0]=0;
+  let rightSum=[];
   rightSum[nums.length-1]=0;
+  for(let i=0;i<nums.length-1;i++){
+    leftSum[i+1]=leftSum[i]+nums[i];
+  }
+  // console.log(leftSum);  //[0,10,14,22]
   for(let i=nums.length-2;i>=0;i--){
     rightSum[i]=rightSum[i+1]+nums[i+1];
   }
-  // console.log(rightSum)
-  let answer=[];
+  // console.log(rightSum); //[15,11,3,0]
+  let ans=[];
   for(let i=0;i<nums.length;i++){
-    answer[i]=Math.abs(leftSum[i]-rightSum[i])
+    ans[i]=Math.abs(leftSum[i]-rightSum[i])
   }
-  return answer;
+  return ans;
 };
